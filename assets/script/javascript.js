5414329b01eacd42bf61c7045d5b4e88b051e034
Peter van der Meulen <peter@picoreclame.nl>
2013-09-03 16:12:25 +0200

    Creates Search and nav (unresponsive as of yet)
    Nav and search are now in the project. the second nav is still missing
    and responsive engines are not up to speed yet.

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,3 +1,35 @@
 // Script file part of FinGraphic copyright Peter van der Meulen and Pico Reclame 2013. Planning on using the contents of my work? contact me at www.github.com/amonfog
 
+$( document ).ready(function(){
+	
+	// navigation to overzicht
+	$('#overzicht-link').click(function(){
+		// remove classes
+		$('#discussie-link').removeClass("nav-active");
+		$('#faq-link').removeClass("nav-active");
+		// add classes
+		$(this).addClass("nav-active");
 
+	});
+
+	// navigation to discussie
+	$('#discussie-link').click(function(){
+		// remove classes
+		$('#overzicht-link').removeClass("nav-active");
+		$('#faq-link').removeClass("nav-active");
+		// add classes
+		$(this).addClass("nav-active");
+
+	});
+
+	// navigation to discussie
+	$('#faq-link').click(function(){
+		// remove classes
+		$('#overzicht-link').removeClass("nav-active");
+		$('#discussie-link').removeClass("nav-active");
+		// add classes
+		$(this).addClass("nav-active");
+
+	});
+
+});
