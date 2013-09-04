100324dfe48d6bba5170d38862a5061bda01c271
Peter van der Meulen <peter@picoreclame.nl>
2013-09-04 12:08:09 +0200

    Working navigation and first page elements
    Responsive is working a bit now, the header is working (.click()
    functions) and random stuff seems to do stuff with stuff

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -4,6 +4,8 @@ $( document ).ready(function(){
 	
 	// navigation to overzicht
 	$('#overzicht-link').click(function(){
+
+		$('.nav-active').css('background', 'none');
 		// remove classes
 		$('#discussie-link').removeClass("nav-active");
 		$('#faq-link').removeClass("nav-active");
@@ -14,10 +16,14 @@ $( document ).ready(function(){
 			$('#overzicht-sub-header').fadeIn();
 		};
 
+		$('.header-container').css('background', '#2ECC71');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #27AE60');
+
 	});
 
 	// navigation to discussie
 	$('#discussie-link').click(function(){
+		$('.nav-active').css('background', 'none');
 		// remove classes
 		$('#overzicht-link').removeClass("nav-active");
 		$('#faq-link').removeClass("nav-active");
@@ -28,10 +34,14 @@ $( document ).ready(function(){
 			$('#overzicht-sub-header').fadeOut();
 		};
 
+		$('.header-container').css('background', '#2ECC71');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #27AE60');
+
 	});
 
-	// navigation to discussie
+	// navigation to faq
 	$('#faq-link').click(function(){
+		$('.nav-active').css('background', 'none');
 		// remove classes
 		$('#overzicht-link').removeClass("nav-active");
 		$('#discussie-link').removeClass("nav-active");
@@ -41,6 +51,9 @@ $( document ).ready(function(){
 		if($('#overzicht-sub-header').is(":visible")) {
 			$('#overzicht-sub-header').fadeOut();
 		};
+
+		$('.header-container').css('background', '#2ECC71');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #27AE60');
 
 	});
 
@@ -58,10 +71,11 @@ $( document ).ready(function(){
 		$(this).addClass("subnav-active");
 
 		$('.header-container').css('background', '#2ECC71');
-		$('.nav-active').css('background', 'url(../images/header/nav-active.png) top repeat-x #27AE60');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #27AE60');
 		$('.overzicht-sub-header').css('background', '#27AE60');
 		$('.header-nav a:hover').css('background', '#27AE60');
-		$('.overzicht-sub-header a:hover').css('color', '#2ECC71');
+		$('.header-search-text').css('color', '#27AE60');
+		$('.header-search-submit').css('background', 'url(assets/images/header/search-bar-01.png) no-repeat right');
 		
 		
 		
@@ -77,11 +91,15 @@ $( document ).ready(function(){
 		// add classes
 		$(this).addClass("subnav-active");
 
+		// css migrations
 		$('.header-container').css('background', '#E67E22');
-		$('.nav-active').css('background', 'url(../images/header/nav-active.png) top repeat-x #D35400');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #D35400');
 		$('.overzicht-sub-header').css('background', '#D35400');
 		$('.header-nav a:hover').css('background', '#D35400');
-		$('.overzicht-sub-header a:hover').css('color', '#E67E22');
+		$('.subnav').css('color', '#FFF');
+		$('.subnav-active').css('color', '#D35400');
+		$('.header-search-text').css('color', '#D35400');
+		$('.header-search-submit').css('background', 'url(assets/images/header/search-bar-02.png) no-repeat right');
 	});
 
 	// bezuinigingen
@@ -89,10 +107,19 @@ $( document ).ready(function(){
 		// remove classes
 		$('#nav-inkomsten').removeClass("subnav-active");
 		$('#nav-consumentenprijzen').removeClass("subnav-active");
-		$('#nav-').removeClass("subnav-active");
+		$('#nav-bevolking').removeClass("subnav-active");
 		$('#nav-financieel').removeClass("subnav-active");
 		// add classes
 		$(this).addClass("subnav-active");
+
+		// css migrations
+		$('.header-container').css('background', '#3498DB');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #2980B9');
+		$('.overzicht-sub-header').css('background', '#2980B9');
+		$('.header-nav a:hover').css('background', '#2980B9');
+		$('.subnav').css('color', '#FFF');
+		$('.header-search-text').css('color', '#2980B9');
+		$('.header-search-submit').css('background', 'url(assets/images/header/search-bar-03.png) no-repeat right');
 
 
 	});
@@ -106,6 +133,15 @@ $( document ).ready(function(){
 		$('#nav-financieel').removeClass("subnav-active");
 		// add classes
 		$(this).addClass("subnav-active");
+
+		// css migrations
+		$('.header-container').css('background', '#E74C3C');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #C0392B');
+		$('.overzicht-sub-header').css('background', '#C0392B');
+		$('.header-nav a:hover').css('background', '#D35400');
+		$('.subnav').css('color', '#FFF');
+		$('.header-search-text').css('color', '#D35400');
+		$('.header-search-submit').css('background', 'url(assets/images/header/search-bar-04.png) no-repeat right');
 	});
 
 	// financieel
@@ -117,6 +153,16 @@ $( document ).ready(function(){
 		$('#nav-bevolking').removeClass("subnav-active");
 		// add classes
 		$(this).addClass("subnav-active");
+
+		// css migrations
+		$('.header-container').css('background', '#F1C40F');
+		$('.nav-active').css('background', 'url(assets/images/header/nav-active.png) top repeat-x #F39C12');
+		$('.overzicht-sub-header').css('background', '#F39C12');
+		$('.header-nav a:hover').css('background', '#F39C12');
+		$('.subnav').css('color', '#FFF');
+		$('.header-search-text').css('color', '#F39C12');
+		$('.header-search-submit').css('background', 'url(assets/images/header/search-bar-05.png) no-repeat right');
+		
 	});
 
 	// smooth scrolling
@@ -124,5 +170,25 @@ $( document ).ready(function(){
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 	});
+
+	// Preloading Images on page load
+	$.fn.preload = function() {
+	    this.each(function(){
+	        $('<img/>')[0].src = this;
+	    });
+	}
+
+	// Usage:
+
+	$(['assets/images/header/search-bar-01.png','assets/images/header/search-bar-02.png','assets/images/header/search-bar-03.png','assets/images/header/search-bar-04.png','assets/images/header/search-bar-05.png']).preload();$.fn.preload = function() {
+	    this.each(function(){
+	        $('<img/>')[0].src = this;
+	    });
+	}
+
+	// // Usage:
+
+	 $(['assets/images/header/search-bar-01.png','assets/images/header/search-bar-02.png','assets/images/header/search-bar-03.png','assets/images/header/search-bar-04.png','assets/images/header/search-bar-05.png']).preload();
+
 
 });
