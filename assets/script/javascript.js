4245d60490f92ecda4e8d30020ee2997f4ddf12e
Peter van der Meulen <peter@picoreclame.nl>
2013-09-04 16:09:01 +0200

    all pages added
    all pages are added click() needs to be replaced with spacetesters on
    scroll()

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,8 +1,6 @@
 // Script file part of FinGraphic copyright Peter van der Meulen and Pico Reclame 2013. Planning on using the contents of my work? contact me at www.github.com/amonfog
 
 $( document ).ready(function(){
-
-	alert('Wil het een beetje lukken? Groetjes, Peter')
 	
 	// navigation to overzicht
 	$('#overzicht-link').click(function(){
